9cd8675fac130419fc63b4b47fc5ae4e011912d8
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-07-30 09:28:38 +0100

    Completed Program.cs , Saving and recollecting the last known settings

--- ConsoleAppTester/Program.cs
+++ ConsoleAppTester/Program.cs
@@ -11,14 +11,81 @@ namespace ConsoleAppTester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Specify full path to the trained TF model");
-            //string modelfile = Console.ReadLine();
-            string modelfile = @"C:\Users\saurabhd\MyTrials\MachineLearnings-2\MNISTpng\PythonTrainer\TrainedMnistTFModel.pb";
+            string modelfile = null;
+            string folderWithMNIST = null;
+            try
+            {
+                modelfile = PromptUserForTrainedModel();
+                folderWithMNIST = PromptUserForMNISTTestImages();
 
-            Console.WriteLine("Specify the top level folder which contains the MNIST test images");
-            //string folderWithMNIST = Console.ReadLine();
-            string folderWithMNIST = @"C:\Users\saurabhd\MyTrials\MachineLearnings\MNIST101\mnist_png\testing";
-            TestMnistImagesUsingTrainedModel(modelfile, folderWithMNIST);
+                //modelfile = @"C:\Users\saurabhd\MyTrials\MachineLearnings-2\MNISTpng\PythonTrainer\TrainedMnistTFModel.pb";
+                //folderWithMNIST = @"C:\Users\saurabhd\MyTrials\MachineLearnings\MNIST101\mnist_png\testing";
+                TestMnistImagesUsingTrainedModel(modelfile, folderWithMNIST);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static string PromptUserForMNISTTestImages()
+        {
+            string lastKnownTestImagesFolder = Properties.Settings.Default.dir_testing;
+            string folderTestImages = null;
+            string yesno = null;
+            if (
+                (string.IsNullOrWhiteSpace(lastKnownTestImagesFolder) == false) &&
+                (System.IO.Directory.Exists(lastKnownTestImagesFolder) == true)
+                )
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Last known test images folder:");
+                Console.WriteLine(lastKnownTestImagesFolder);
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Do you want to use the last selected Test images directory (y/n)?");
+                yesno = Console.ReadLine();
+                if (yesno == "y")
+                {
+                    folderTestImages = lastKnownTestImagesFolder;
+                    return folderTestImages;
+                }
+            }
+            Console.WriteLine("Specify full path to Test images directory");
+            folderTestImages = Console.ReadLine();
+            Properties.Settings.Default.dir_testing = folderTestImages;
+            Properties.Settings.Default.Save();
+            return folderTestImages;
+        }
+
+        private static string PromptUserForTrainedModel()
+        {
+            string lastModelFile = Properties.Settings.Default.file_trainedmodel;
+            string yesno = null;
+            string modelfile = null;
+            if (
+                (string.IsNullOrWhiteSpace(lastModelFile) == false) &&
+                (System.IO.File.Exists(lastModelFile) == true)
+                )
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Last known model file:");
+                Console.WriteLine(lastModelFile);
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Do you want to use the last known model file (y/n)?");
+                yesno = Console.ReadLine();
+                if (yesno.ToLower() == "y")
+                {
+                    modelfile = lastModelFile;
+                    return modelfile;
+                }
+            }
+                        
+            Console.WriteLine("Specify absolute path to the trained TF model:");
+            Console.WriteLine("----------------------------------------------");
+            modelfile = Console.ReadLine();
+            Properties.Settings.Default.file_trainedmodel = modelfile;
+            Properties.Settings.Default.Save();
+            return modelfile;
         }
 
         private static void TestMnistImagesUsingTrainedModel(string modelfile, string folderWithMNIST)
